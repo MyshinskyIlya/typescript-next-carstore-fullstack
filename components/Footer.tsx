d4ceedfb5f9137404ac149c7955125171939db96
MyshinskyIlya <myshinskiyyy@gmail.com>
2023-12-09 15:16:54 +0400

    "added footer"

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { CarLogo } from ".";
+import { footerLinks } from "@/constants";
+import Link from "next/link";
 
 const Footer = () => {
     return (
@@ -12,7 +14,35 @@ const Footer = () => {
                         All rights reserved &copy;
                     </p>
                 </div>
-                <div className="footer__links">links</div>
+                <div className="footer__links">
+                    {footerLinks.map((link) => (
+                        <div key={link.title} className="footer__link">
+                            <h3 className="font-bold">{link.title}</h3>
+                            {link.links.map((item) => (
+                                <Link
+                                    key={item.title}
+                                    href={item.url}
+                                    className="text-[#666666]"
+                                >
+                                    {item.title}
+                                </Link>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="footer__copyrights flex items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10 sm:gap-2">
+                <div className="sm:mr-0 mr-8">
+                    @2023 CarHub. All Rights Reserved
+                </div>
+                <div className="footer__copyrights-link">
+                    <Link href={"/"} className="text-[#666666]">
+                        Privacy Policy
+                    </Link>
+                    <Link href={"/"} className="text-[#666666]">
+                        Terms of Use
+                    </Link>
+                </div>
             </div>
         </footer>
     );
